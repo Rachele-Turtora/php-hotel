ea5a794ab5db0d9be88d7dc809d7000c25c3813f
Rachele-Turtora <racheleturtora21@gmail.com>
2024-06-18 14:59:22 +0200

    [UPD] hotels table

--- index.php
+++ index.php
@@ -47,20 +47,41 @@ $hotels = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Hotels</title>
 </head>
 
 <body>
-    <h1>Hotels</h1>
-    <?php
-    foreach ($hotels as $hotel) {
-        echo "<br>";
-        foreach ($hotel as $key => $element) {
-            echo "$key - $element";
-            echo "<br>";
-        }
-    }
-    ?>
+    <table class="table">
+        <thead>
+            <tr>
+                <?php foreach ($hotels[0] as $key => $hotel) : ?>
+                    <th scope="col"><?php echo $key ?></th>
+                <?php endforeach; ?>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($hotels as $hotel) : ?>
+                <tr>
+                    <th scope="row"><?php echo $hotel['name'] ?></th>
+                    <?php foreach (array_slice($hotel, 1) as $key => $element) : ?>
+                        <td>
+                            <?php # echo $key === 'parking' ? ($element ? '&#10003;' : '&#x2717;') : $element; 
+                            if ($key === 'parking') {
+                                if ($element == true) {
+                                    $element = '&#10003;';
+                                } else {
+                                    $element = '&#x2717;';
+                                }
+                            }
+                            echo $element;
+                            ?>
+                        </td>
+                    <?php endforeach; ?>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 </body>
 
 </html>
